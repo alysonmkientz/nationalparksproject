8a27e0a1c014703c9cb7addf13dff34b2b88150b
Aly Kientz <alysonmkientz@gmail.com>
2019-01-05 13:58:16 -0600

    JavaScript worked on

---
+++ js.js
@@ -0,0 +1,51 @@
+var apiKey = 'rgsAqsFxMxT9w41rIuv5Jt4Uk7Z3Z8rPaNqEUzll';
+
+function formatQueryParams(params){
+    const queryItems = Object.keys(params)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+}
+
+function displayResults(responseJson, maxResults){
+    $('.results').empty();
+    console.log(responseJson);
+    for (let i = 0; i < responseJson.length; i++){
+        $('.results').append(
+            `<li>
+            <h2><a href='${responseJson[i].url}'>${responseJson[i].fullName}</a></h2>
+            </li>
+            <li>
+            <p>${responseJson[i].description}</p>
+            </li>`
+        )
+    }
+}
+
+function getParks(query, maxResults=10){
+    const params = {
+        stateCode: query,
+        limit: max,
+    };
+
+    const queryString = formatQueryParams(params);
+    const url = searchUrl + 'parks?' + queryString;
+    console.log(url);
+
+    const options = {
+        headers: new Headers({
+            "X-Api-Key": apiKey
+        })
+    };
+    
+    fetch(url, options)
+        .then(response => {
+            if(response.ok){
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson, maxResults))
+        .catch(error => {
+            $('body').append(`Error placeholder text`);
+        });
+}
